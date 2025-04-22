a65cb5d4fd76f3285c30083448a902f8267ccca4
Alonso Suarez <alonchb@gmail.com>
2025-04-21 22:37:04 -0700

    fix aws install step

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_ssm_document" "service" {
                 echo "AWS CLI not found. Installing..."
                 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
                 unzip awscliv2.zip
-                sudo ./aws/install
+                sudo ./aws/install --update
                 rm -rf awscliv2.zip aws
               fi
               
